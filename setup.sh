3605045f8df7b7e056d3c91be2421b2f41f9b5b5
Gaku <neko0774@gmail.com>
2021-03-06 19:17:58 +0100

    change

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ read input
 mkdir $input
 if [-e {$input}]; then
     echo "directory {$input} already exits"
-    fi
-cp sample/main.cpp {$ipnut}/main.cpp
-cp sample/main.py {$ipnut}/main.py
+    fi:
+cp sample/main.cpp !/home/neko/math/{$ipnut}/main.cpp
+cp sample/main.py !/home/neko/math/{$ipnut}/main.py
 cd {$input}
