7b705b3a9266043e889387266d2a2957d7c6b454
Gaku <neko0774@gmail.com>
2021-03-07 21:00:40 +0100

    solved 12
    11 は実装が面倒だったので後回し

--- 12/main.cpp
+++ 12/main.cpp
@@ -1,10 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-int i,j;
 ll ans;
 ll mod=pow(10, 9)+9;
 
-int main(){
+int divisions(ll x){
+    ll j=1;//x = 1 return flase value though
+    int cnt=0;
+    while (j*j<=x){
+        if(x%j==0){
+            if(x%j==j) cnt++;
+            else cnt+=2;
+        };
+        j++;
+    }
+    return cnt;
+}
 
+int main(){
+    ll num=1;
+    ll ans=1;
+    while(divisions(ans)<=500){
+        num++;
+        ans+=num;
+    }
+    cout << num << "|" << ans <<endl;
+    cout << divisions(7676500) << endl;
+    ll a = 7676500;
+    cout << divisions(a) << endl;
 }
